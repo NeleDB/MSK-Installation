29210e5b2a5bb73b8d90ccdb653c505ade5ad9ee
lottebijlsma <bijlsmalotte@gmail.com>
2017-06-13 23:37:25 +0200

    fixed look for ipad mini

--- src/js/containers/Questions.jsx
+++ src/js/containers/Questions.jsx
@@ -8,17 +8,16 @@ const Questions = ({currentQuestion, playersLeft, players}) => {
   return (
     <div className='startpage'>
       <div className='players'>
-        <img src='../../assets/img/nrOfPlayers.png' alt='nrOfPlayers'></img>
+        <img src='../../assets/svg/people-icon.svg' height='20' alt='nrOfPlayers'></img>
         <p className='playernumber'>{players}</p>
       </div>
       <div className='homebody'>
         <div className='header'>
           <h1>Wem hier prijs!</h1>
           <p className='desc'>Selecteer een vree wijs kunstwerk en ontdek jouw kunstmatch! </p>
+          {/* <p>Spelers die nog moeten antwoorden: {playersLeft}</p> */}
         </div>
         <Question id={currentQuestion} />
-        <p>players who need to answer {playersLeft}</p>
-
       </div>
     </div>
   );
